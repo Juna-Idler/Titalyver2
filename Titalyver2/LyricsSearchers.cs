994eb15dfef2eac376531ed2752d7efb6780414b
Juna <juna.idler@gmail.com>
2021-09-26 09:14:58 +0900

    %基本情報%にファイル名処理を施してなかった。あと.を最後だけ変換に変更。

--- Titalyver2/LyricsSearchers.cs
+++ Titalyver2/LyricsSearchers.cs
@@ -76,7 +76,9 @@ namespace Titalyver2
             _ = sb.Replace("%directoryname%", directoryname);
             _ = sb.Replace("%filename%", filename).Replace("%filename_ext%", filename_ext).Replace("%path%", filepath);
             _ = sb.Replace("%mydocuments%", Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
-            _ = sb.Replace("%title%", title).Replace("%artists%", string.Join(artists_separator, artists)).Replace("%album%", album);
+            _ = sb.Replace("%title%", ReplaceInvalidFileNameChars(title))
+                  .Replace("%artists%", ReplaceInvalidFileNameChars(string.Join(artists_separator, artists)))
+                  .Replace("%album%", ReplaceInvalidFileNameChars(album));
 
             s = sb.ToString();
             string r = "";
@@ -118,7 +120,8 @@ namespace Titalyver2
             filename = filename.Replace('<', '＜');
             filename = filename.Replace('>', '＞');
             filename = filename.Replace('|', '｜');
-            filename = filename.Replace('.', '．');
+
+            filename = Regex.Replace(filename,@"\.$","．");
             return filename;
         }
 
